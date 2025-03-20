b6941f17acf206eb4b7b83333450b66dfa3d8275
Christian Ramirez <christian.ramirez1@umassmed.edu>
2025-03-20 11:41:47 -0400

    color by strand

--- plot_avg_profiles.py
+++ plot_avg_profiles.py
@@ -6,19 +6,23 @@ from matplotlib.lines import Line2D
 from tqdm import tqdm
 import umap
 import pybigtools
+from gtfparse import read_gtf
 import pickle
 from multiprocessing import Pool
 
-def process_ccre_data(input_file="/data/projects/encode/Registry/V4/GRCh38/Cell-Type-Specific/Individual-Files/ENCFF414OGC_ENCFF806YEZ_ENCFF849TDM_ENCFF736UDR.bed", 
+print('Imported dependencies successfully!')
+
+def process_ccre_data(cell_type_specific_ccres="/data/projects/encode/Registry/V4/GRCh38/Cell-Type-Specific/Individual-Files/ENCFF414OGC_ENCFF806YEZ_ENCFF849TDM_ENCFF736UDR.bed",
+                     pls_gene_list="/data/projects/encode/Registry/V4/GRCh38/PLS-Gene-List.txt",
+                     gencode_gtf="/data/common/genome/gencode.v47.basic.annotation.gtf",
                      output_file="ccre_df.tsv"):
     """Process K562-specific cCREs, filtering for relevant types."""
     main_chromosomes = [f"chr{i}" for i in range(1, 23)]
 
-    ccre_df = pd.read_csv(input_file, sep='\t', header=None, engine='python')
+    ccre_df = pd.read_csv(cell_type_specific_ccres, sep='\t', header=None)
     filtered_df = ccre_df[
-        (ccre_df[9].isin(['PLS', 'dELS', 'pELS'])) & 
-        (ccre_df[0].isin(main_chromosomes))
-    ]
+        (ccre_df[9].isin(['PLS', 'dELS'])) & 
+        (ccre_df[0].isin(main_chromosomes))]
     
     result_df = pd.DataFrame({
         'chrom': filtered_df[0],
@@ -27,7 +31,14 @@ def process_ccre_data(input_file="/data/projects/encode/Registry/V4/GRCh38/Cell-
         'rDHS': filtered_df[3],
         'cCRE_type': filtered_df[9]
     })
-    
+    pls_gene_list = pd.read_csv(pls_gene_list, sep='\t', header=None)
+    pls_gene_list.columns = ['rDHS', 'gene_id']
+    result_df = result_df.merge(pls_gene_list, how='left', on='rDHS')
+
+    gtf_df = read_gtf(gencode_gtf, result_type='pandas')
+    genes_df = gtf_df[gtf_df['feature'] == 'gene'][['gene_id', 'strand']]
+    result_df = result_df.merge(genes_df, on='gene_id', how='left')
+
     result_df.to_csv(output_file, sep='\t', index=False)
     return result_df
 
@@ -106,31 +117,55 @@ def plot_average_profiles(signal_matrix, ccre_df, assay_names, output_dir="avera
         plt.savefig(f"{output_dir}/{assay_name}_profile.png", dpi=300)
         plt.close()
 
-def run_umap_visualization(signal_matrix, ccre_df, output_dir="umap_plots"):
-    os.makedirs(output_dir, exist_ok=True)
-    num_samples = signal_matrix.shape[0]
-    signal_2d = signal_matrix.reshape(num_samples, -1)
-    reducer = umap.UMAP(n_neighbors=30, min_dist=0.1, verbose=True, metric='cosine')
-    embedding = reducer.fit_transform(signal_2d)
-    with open('embedding.pkl', 'wb') as f:
-        pickle.dump(embedding, f)
+def run_umap_visualization(signal_matrix, ccre_df, output_dir="umap_plots", metric=None, color_by='ccre', load_from_file=False):
+    if load_from_file:
+        with open('embedding.pkl', 'rb') as f:
+            embedding = pickle.load(f)
+    else:
+        print(f'Signal matrix shape: {signal_matrix.shape}')
+        os.makedirs(output_dir, exist_ok=True)
+        num_samples = signal_matrix.shape[0]
+        signal_2d = signal_matrix.reshape(num_samples, -1)
+        print(f'Flattened signal matrix shape: {signal_2d.shape}')
+        reducer = umap.UMAP(n_neighbors=30, min_dist=0.1, verbose=True, metric=metric)
+        embedding = reducer.fit_transform(signal_2d)
+        with open('embedding.pkl', 'wb') as f:
+            pickle.dump(embedding, f)
 
-    ccre_types = ccre_df['cCRE_type'].values
-    color_map = {'PLS' : '#FF0000', 
-               'pELS' : '#FFA700', 
-               'dELS' : '#FFCD00'}
-    colors = [color_map.get(ctype, 'gray') for ctype in ccre_types]
-    plt.figure(figsize=(10, 8))
-    plt.scatter(embedding[:, 0], embedding[:, 1], c=colors, alpha=0.25, s=2)
-    unique_types = np.unique(ccre_types)
-    legend_elements = [Line2D([0], [0], marker='o', color='w', markerfacecolor=color_map.get(ctype, 'gray'), 
-                             markersize=10, label=ctype) for ctype in unique_types]
-    plt.legend(handles=legend_elements)
+    if color_by == 'strand':
+        strands = ccre_df['strand'].astype(str)
+        color_map = {
+            '+': '#FF4B4B',
+            '-': '#4B4BFF',
+            'nan': '#CCCCCC'}
+        colors = [color_map[strand] for strand in strands]
+        plt.figure(figsize=(10, 8))
+        plt.scatter(embedding[:, 0], embedding[:, 1], c=colors, alpha=0.25, s=2)
+        legend_elements = [
+            Line2D([0], [0], marker='o', color='w', markerfacecolor=color_map['+'], 
+                markersize=10, label='+ strand'),
+            Line2D([0], [0], marker='o', color='w', markerfacecolor=color_map['-'], 
+                markersize=10, label='- strand'),
+            Line2D([0], [0], marker='o', color='w', markerfacecolor=color_map['nan'], 
+                markersize=10, label='Unknown')]
+        plt.legend(handles=legend_elements)
+    elif color_by == 'ccre':
+        ccre_types = ccre_df['cCRE_type'].values
+        color_map = {'PLS' : '#FF0000', 
+                'pELS' : '#FFA700', 
+                'dELS' : '#FFCD00'}
+        colors = [color_map.get(ctype, 'gray') for ctype in ccre_types]
+        plt.figure(figsize=(10, 8))
+        plt.scatter(embedding[:, 0], embedding[:, 1], c=colors, alpha=0.25, s=2)
+        unique_types = np.unique(ccre_types)
+        legend_elements = [Line2D([0], [0], marker='o', color='w', markerfacecolor=color_map.get(ctype, 'gray'), 
+                                markersize=10, label=ctype) for ctype in unique_types]
+        plt.legend(handles=legend_elements)
     
     plt.title('UMAP of Genomic Signals')
     plt.xlabel('UMAP 1')
     plt.ylabel('UMAP 2')
-    plt.savefig(f"{output_dir}/umap_ccre_types.png", dpi=300)
+    plt.savefig(f"{output_dir}/umap_{color_by}_{metric}.png", dpi=300)
     plt.close()
 
 def main():
@@ -138,7 +173,7 @@ def main():
     bigwig_files, assay_names = find_bigwig_files()
     signal_matrix = build_signal_matrix(ccre_df, bigwig_files)
     # plot_average_profiles(signal_matrix, ccre_df, assay_names)
-    run_umap_visualization(signal_matrix, ccre_df)
+    run_umap_visualization(signal_matrix, ccre_df, color_by='ccre', metric='cosine')
 
 if __name__ == "__main__":
     main()
